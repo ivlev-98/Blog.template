410fd29e2e95e415c24507fbcba6d25a4a7a9ab2
Александр Ивлев <aivlev.98@yandex.ru>
2022-04-02 11:06:11 +0300

    Change fontawesome dom.i2svg to native svg icons

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import './stylus/all.styl';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { navToggler } from './ts/navToggler';
-library.add(faBars);
-dom.i2svg()
-    .then(() => {
-        const headerNav: navToggler = new navToggler({
-            'triggers': ['.site-header__controls'],
-            'toggleItems': ['.site-header__nav', '#site-overlay'],
-            'activeClass': 'active'
-        })
-        headerNav.watch();
-        const rightNav: navToggler = new navToggler({
-            'triggers': ['#menuTrigger'],
-            'toggleItems': ['#site-overlay2', '.container', '.site-header'],
-            'activeClass': 'active'
-        })
-        rightNav.watch();
-    });
+const headerNav: navToggler = new navToggler({
+    'triggers': ['.site-header__controls'],
+    'toggleItems': ['.site-header__nav', '#site-overlay'],
+    'activeClass': 'active'
+})
+headerNav.watch();
+const rightNav: navToggler = new navToggler({
+    'triggers': ['#menuTrigger'],
+    'toggleItems': ['#site-overlay2', '.container', '.site-header'],
+    'activeClass': 'active'
+})
+rightNav.watch();
